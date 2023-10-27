b2af497a72feb486d910181813efd363e446b36e
Juan Pablo Chavez Arteaga <juanpablo.chavez@training.assuresoft.com>
2023-10-27 18:50:05 -0400

    url format hotfix

--- src/routes/client.route.js
+++ src/routes/client.route.js
@@ -14,7 +14,7 @@ const router = Router()
 router.get('/get-all-clients-mongoose', getAllClientsMongoose)
 router.post('/create-client-mongoose', createClientMongoose)
 router.put('/update-client-mongoose/:id', updateClientMongoose)
-router.delete('/delete-client-mongoose', deleteClientMongoose)
+router.delete('/delete-client-mongoose/:id', deleteClientMongoose)
 
 router.post('/create-client-mongodb', createClientMongoDB)
 router.put('/update-client-mongoose', updateClientMongoDB)
