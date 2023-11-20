f2d4124c5b5c1b2158ec11a62b7c0b75ce46911b
Juan Pablo Chavez Arteaga <juanpablo.chavez@training.assuresoft.com>
2023-11-20 11:04:02 -0400

    Console log added in case db connection fails

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -5,6 +5,8 @@ config()
 
 const mongooseClient = mongoose.connect(process.env.MONGO_URL as string).then(() => {
   console.log('success connect to mongoose')
-})
+}).catch(err => {
+  console.error('Error connecting to mongoose:', err);
+});
 
 export default mongooseClient
