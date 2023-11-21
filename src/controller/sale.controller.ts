db794e474559ee0e227487c6b7cde909a29e5363
Juan Pablo Chavez Arteaga <juanpablo.chavez@training.assuresoft.com>
2023-11-21 11:18:56 -0400

    Sale endpoints now use classes

--- src/controller/sale.controller.ts
+++ src/controller/sale.controller.ts
@@ -1,82 +1,90 @@
 import { Request, Response } from 'express'
-import {
-  getAllSalesRepository,
-  createSaleRepository,
-  updateSaleRepository,
-  deleteSaleRepository,
-  clientSalesTotalRepository,
-  salesTotalByItemRepository
-} from '../repositories/sale.repository'
+import SaleRepository from '../repositories/sale.repository'
 
-export const getAllSales = async (req: Request, res: Response) => {
-  try {
-    const sales = await getAllSalesRepository()
-    res.status(200).json(sales)
-  } catch {
-    res.status(500).json({ message: 'Something went wrong' })
+export default class SaleController {
+  private repository: SaleRepository
+
+  public constructor(repository: SaleRepository) {
+    this.repository = repository
+
+    this.getAllSales = this.getAllSales.bind(this)
+    this.createSale = this.createSale.bind(this)
+    this.updateSale = this.updateSale.bind(this)
+    this.deleteSale = this.deleteSale.bind(this)
+    this.clientSalesTotal = this.clientSalesTotal.bind(this)
+    this.salesTotalByItem = this.salesTotalByItem.bind(this)
   }
-}
 
-export const createSale = async (req: Request, res: Response) => {
-  try {
-    const dbResponse = await createSaleRepository(req.body)
-    return res.json(dbResponse)
-  } catch {
-    res.status(500).json({ message: 'Something went wrong' })
-  }
-}
-
-export const updateSale = async (req: Request, res: Response) => {
-  try {
-    const dbResponse = await updateSaleRepository(
-      req.params.id,
-      req.body
-    )
-    return res.json(dbResponse)
-  } catch {
-    res.status(500).json({ message: 'Something went wrong' })
-  }
-}
-
-export const deleteSale = async (req: Request, res: Response) => {
-  try {
-    const dbResponse = await deleteSaleRepository(
-      req.params.id
-    )
-    return res.json(dbResponse)
-  } catch {
-    res.status(500).json({ message: 'Something went wrong' })
-  }
-}
-
-export const clientSalesTotal = async (req: Request, res: Response) => {
-  try {
-    const { id } = req.params
-    const dbResponse = await clientSalesTotalRepository(id)
-
-    const response = {
-      clientId: id,
-      total: dbResponse
+  async getAllSales(req: Request, res: Response) {
+    try {
+      const sales = await this.repository.getAllSales()
+      res.status(200).json(sales)
+    } catch {
+      res.status(500).json({ message: 'Something went wrong' })
     }
-
-    return res.json(response)
-  } catch {
-    res.status(500).json({ message: 'Something went wrong' })
   }
-}
 
-export const salesTotalByItem = async (req: Request, res: Response) => {
-  try {
-    const { item } = req.params
-    const dbResponse = await salesTotalByItemRepository(item)
-
-    const response = {
-      item: item,
-      total: dbResponse
+  async createSale(req: Request, res: Response) {
+    try {
+      const dbResponse = await this.repository.createSale(req.body)
+      return res.json(dbResponse)
+    } catch {
+      res.status(500).json({ message: 'Something went wrong' })
     }
+  }
 
-    return res.json(response)
-  } catch {
-    res.status(500).json({ message: 'Something went wrong' })
+  async updateSale(req: Request, res: Response) {
+    try {
+      const dbResponse = await this.repository.updateSale(
+        req.params.id,
+        req.body
+      )
+      return res.json(dbResponse)
+    } catch {
+      res.status(500).json({ message: 'Something went wrong' })
+    }
+  }
+
+  async deleteSale(req: Request, res: Response) {
+    try {
+      const dbResponse = await this.repository.deleteSale(
+        req.params.id
+      )
+      return res.json(dbResponse)
+    } catch {
+      res.status(500).json({ message: 'Something went wrong' })
+    }
+  }
+
+  async clientSalesTotal(req: Request, res: Response) {
+    try {
+      const { id } = req.params
+      const dbResponse = await this.repository.clientSalesTotal(id)
+
+      const response = {
+        clientId: id,
+        total: dbResponse
+      }
+
+      return res.json(response)
+    } catch {
+      res.status(500).json({ message: 'Something went wrong' })
+    }
+  }
+
+  async salesTotalByItem(req: Request, res: Response) {
+    try {
+      const { item } = req.params
+      const dbResponse = await this.repository.salesTotalByItem(item)
+
+      const response = {
+        item: item,
+        total: dbResponse
+      }
+
+      return res.json(response)
+    } catch {
+      res.status(500).json({ message: 'Something went wrong' })
+    }
   }
 }
